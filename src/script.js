4dab2ade46a04a0e5e3a373b221c4bb60096c809
nugmara <dagmarag0@gmail.com>
2024-04-11 20:05:54 +0200

    Work with LookAt method

--- src/script.js
+++ src/script.js
@@ -12,12 +12,25 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+// Position
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = 1 
 mesh.position.set(0.7, -0.6, 1)
 
 scene.add(mesh)
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2,  0.5, 0.5)
+
+// Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
 
 // Axis Helper
 const axisHelper = new THREE.AxesHelper(2)
@@ -37,6 +50,8 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 
 scene.add(camera)
+
+camera.lookAt(mesh.position)
 
 /**
  * Renderer
