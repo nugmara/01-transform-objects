5d403bf057c650a31179c8e7b108e5cb39b1f802
nugmara <dagmarag0@gmail.com>
2024-04-11 20:14:35 +0200

    Add a group of objects

--- src/script.js
+++ src/script.js
@@ -9,28 +9,28 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+scene.add(group)
 
-// Position
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1 
-mesh.position.set(0.7, -0.6, 1)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cube1)
 
-scene.add(mesh)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.x = -2
+group.add(cube2)
 
-// Scale
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
-mesh.scale.set(2,  0.5, 0.5)
-
-// Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x000ff})
+)
+cube3.position.x = 2
+group.add(cube3)
 
 // Axis Helper
 const axisHelper = new THREE.AxesHelper(2)
@@ -50,8 +50,6 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 
 scene.add(camera)
-
-camera.lookAt(mesh.position)
 
 /**
  * Renderer
